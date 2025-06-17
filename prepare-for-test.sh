7ed9f62862770cbe5685ee41f6dfa849174d1c3b
Victor <2417328+vrobinson@users.noreply.github.com>
2025-06-17 17:23:31 -0400

    Rename docker-compose.yml to docker-compose.template

--- prepare-for-test.sh
+++ prepare-for-test.sh
@@ -5,6 +5,7 @@ destination_dir=test/$container_name/.devcontainer
 
 mkdir -p $destination_dir
 cp -r "$source_dir"/. "$destination_dir"
+mv "$destination_dir/docker-compose.template" "$destination_dir/docker-compose.yml"
 if [ "$copy_env_file" = "true" ]; then
   cp "test/$container_name/.devcontainer/.env.example" "test/$container_name/.devcontainer/.env"
 fi
